47ebd651d9388001a34803a92de1d0d14341846f
Lavínia Paganini <laviniapaganini@gmail.com>
2021-07-06 00:30:52 -0300

    :wrench: Add gatsby config again :wrench:

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Lavínia Paganini',
+    siteUrl: `https://www.lfp2.github.io`,
+  },
+  plugins: ['gatsby-plugin-sitemap', 'gatsby-plugin-styled-components'],
+}
