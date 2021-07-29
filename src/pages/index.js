cdac63067a80d1df9e2a81fc798d64c0d820b568
Lavínia Paganini <laviniapaganini@gmail.com>
2021-07-29 16:00:38 -0300

    :construction: Add a brief description about myself :construction:

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
 import * as React from 'react'
 
 const HomePage = () => {
-  return <h1>Home Page</h1>
+  return (
+    <>
+   <h1>Home Page</h1>
+   <p>Hello! My name is Lavínia Paganini, I'm a Computer Science Master's student in the <a href="https://portal.cin.ufpe.br/" target="_blank" rel="noreferrer external">Centro de Informática</a> at <a href="https://www.ufpe.br/" target="_blank" rel="noreferrer external">Universidade Federal de Pernambuco (UFPE).</a> I currently live in Recife, Brazil.</p>
+   <h2>Research Interests</h2>
+   <p>Human aspects of Software Development</p>
+   <p>Software Engineer</p>
+   <p>Gender diversity</p>
+   <p>Time-bounded events</p>
+   </>
+  )
 }
 
 export default HomePage
