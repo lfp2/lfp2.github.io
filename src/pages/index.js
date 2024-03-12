44a62bd17837cea31ba026981186f391310a4929
lfp2 <laviniapaganini@gmail.com>
2024-03-12 16:08:18 +0100

    feat: update project pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,17 @@ import * as React from 'react'
 const HomePage = () => {
   return (
     <>
-   <h1>Home Page</h1>
-   <p>Hello! My name is Lavínia Paganini, I'm a Computer Science Master's student in the <a href="https://portal.cin.ufpe.br/" target="_blank" rel="noreferrer external">Centro de Informática</a> at <a href="https://www.ufpe.br/" target="_blank" rel="noreferrer external">Universidade Federal de Pernambuco (UFPE).</a> I currently live in Recife, Brazil.</p>
-   <h2>Research Interests</h2>
-   <p>Human aspects of Software Development</p>
-   <p>Software Engineer</p>
-   <p>Gender diversity</p>
-   <p>Time-bounded events</p>
-   </>
+      <h1>Home Page</h1>
+      <p>Hello! My name is Lavínia Paganini, I am a PhD Candidate at the <a href="https://www.tue.nl/en/" target="_blank" rel="noreferrer external">Eindhoven University of Technology</a>.</p>
+      <p>I received my Computer Science Master's degree from <a href="https://portal.cin.ufpe.br/" target="_blank" rel="noreferrer external">Centro de Informática</a> at <a href="https://www.ufpe.br/" target="_blank" rel="noreferrer external">Universidade Federal de Pernambuco (UFPE).</a></p>
+      <p> I currently live in The Netherlands.</p>
+      <h2>Research Interests</h2>
+      <p>Human aspects of Software Development</p>
+      <p>Software Engineering</p>
+      <p>Open-Source Software</p>
+      <p>Gender diversity</p>
+      <p>Time-bounded events</p>
+    </>
   )
 }
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,40 @@
 import * as React from 'react'
+import styled from 'styled-components'
+import { StaticImage } from 'gatsby-plugin-image'
+
+const Square = styled.div`
+min-width: 200px;
+max-width: 500px;
+min-height: 200px;
+display: flex;
+flex-wrap: wrap;
+flex-direction: column;
+background-color: ${({ theme }) => theme.background};
+box-shadow: rgba(0, 0, 0, 0.08) 0px 4px 12px;
+`
+const Title = styled.p`
+  flex: 2;
+  font-weight: bold;
+`
+const Description = styled.p`
+  flex: 1;
+  margin: 6px 12px 6px 12px;
+  text-align: justify;
+`
+
+
+
 
 const ProjectsPage = () => {
   return (
     <>
       <h1>Projects</h1>
-      <p>Under construction</p>
+      <Square>
+        <StaticImage src="../images/hack_grrrl.jpg" alt="Picture with Hack GRRRL mentors hugging" placeholder="blurred" layout="constrained" width={200} aspectRatio={16 / 9} quality={100}/>
+        <Title>Hack GRRRL</Title>
+        <Description>Hack GRRRL is a women-focused hackathon, an time-bounded event with duration between 24 to 48 hours, where participants are part of multidisciplinary teams and create a tech solution for a problem. I co-founded and organized the first Hack GRRRL in Recife with 231 enrollments and 26 participants in 2019. We achieved 55 participants on the online version in 2021, where all the groups finished the hackathon with an MVP.</Description>
+        <Description>Learn more about Hack GRRRL <a href="https://hackgrrrl.com/" target="_blank" rel="noreferrer external">here.</a></Description>
+      </Square>
     </>
   )
 }
