0d05f10f2bc2d9fa0d9d67b9a080b637f39ea40d
lfp2 <laviniapaganini@gmail.com>
2024-03-12 17:17:31 +0100

    feat: add field day in my projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -35,6 +35,12 @@ const ProjectsPage = () => {
         <Description>Hack GRRRL is a women-focused hackathon, an time-bounded event with duration between 24 to 48 hours, where participants are part of multidisciplinary teams and create a tech solution for a problem. I co-founded and organized the first Hack GRRRL in Recife with 231 enrollments and 26 participants in 2019. We achieved 55 participants on the online version in 2021, where all the groups finished the hackathon with an MVP.</Description>
         <Description>Learn more about Hack GRRRL <a href="https://hackgrrrl.com/" target="_blank" rel="noreferrer external">here.</a></Description>
       </Square>
+      <Square>
+        <StaticImage src="../images/field_day.jpg" maxWidth={400} aspectRatio={21/9} quality={100}/>
+        <Title>GitHub Field Day Brasil</Title>
+        <Description>Field Day brings together student leaders from different regional communities to hang out, become friends, learn from each other's mistakes and successes, and solve shared problems. I organized the first GitHub Field day in Brasil, which had 26 participants from different tech communities in Recife (e.g., Afro Python, PyLadies, UXCO).</Description>
+        <Description>Learn more about GitHub Field Day <a href="https://githubfieldday.com/" target="_blank" rel="noreferrer external">here.</a></Description>
+      </Square>
     </>
   )
 }
